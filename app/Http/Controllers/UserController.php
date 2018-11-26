13e9083663784134f3db00390865e4f90641a026
Max <contact@maxsnow.me>
2018-11-26 17:52:19 +1100

    Add user stuff

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,20 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     public static $model = User::class;
+
+    public function post(Request $request)
+    {
+        $user = parent::post($request);
+
+        // event
+
+        // email
+
+        return $this->response->item($user, $this->getTransformer())->setStatusCode(201);
+    }
 }
